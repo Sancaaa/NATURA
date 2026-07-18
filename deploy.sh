200088b76398a8a593251096f9eb63b4e2788ad5
Sancaaa <luffybuahkaret@gmail.com>
2026-07-18 15:16:07 +0000

    fix: dupe deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -17,36 +17,7 @@ echo "==> Build Next.js"
 npm run build
 
 echo "==> Restart PM2"
-pm2 restart natura --update-env
-
-echo "==> Save PM2"
-pm2 save
-
-echo ""
-echo "=================================="
-echo "Deploy selesai!"
-echo "=================================="
-
-pm2 status#!/bin/bash
-
-set -e
-
-APP_DIR="$HOME/test/NATURA"
-
-echo "==> Masuk ke project"
-cd "$APP_DIR"
-
-echo "==> Git pull"
-git pull
-
-echo "==> Install dependencies"
-npm install
-
-echo "==> Build Next.js"
-npm run build
-
-echo "==> Restart PM2"
-pm2 restart natura --update-env
+pm2 restart tomo --update-env
 
 echo "==> Save PM2"
 pm2 save
